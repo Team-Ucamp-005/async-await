9bfd41e0163304b398416ff8ac7d20191a249809
Alfredo Hernandez Noriega <alfredo.hernandez@kushkipagos.com>
2022-04-20 11:41:42 -0500

    change main

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
-const buscarFrase = () => {
-  const res = fetch("https://api.chucknorris.io/jokes/random");
-
-  console.log(res)
-
-  const frase = res.json();
-
-  console.log(frase);
-};
-
-buscarFrase();
+console.log('Hola Mundo');
