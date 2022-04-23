9081bb2027a6408faf5c694cc7b170545b4d5361
Alfredo Hernandez Noriega <alfredo.hernandez@kushkipagos.com>
2022-04-23 10:09:35 -0500

    ejercicio guiado 1

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log('Hola Mundo');
+
+
+const imprimirPalabra = (palabra) => {
+  console.log(palabra)
+}
+
+const saludar = (funcionCallback, str) => {
+  const palabra = str.toUpperCase()
+  return funcionCallback(palabra)
+}
+
+saludar(imprimirPalabra, 'holi crayoli')
