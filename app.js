9b2275cca0097dee29cc05a3406c49229ef8a29d
Alfredo Hernandez Noriega <alfredo.hernandez@kushkipagos.com>
2022-04-23 11:21:40 -0500

    ejercicio 2

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
 console.log('Hola Mundo');
 
 
-const imprimirPalabra = (palabra) => {
-  console.log(palabra)
-}
+// const imprimirPalabra = (palabra) => {
+//   console.log(palabra)
+// }
 
-const saludar = (funcionCallback, str) => {
-  const palabra = str.toUpperCase()
-  return funcionCallback(palabra)
-}
+// const saludar = (funcionCallback, str) => {
+//   const palabra = str.toUpperCase()
+//   return funcionCallback(palabra)
+// }
 
-saludar(imprimirPalabra, 'holi crayoli')
+// saludar(imprimirPalabra, 'holi crayoli')
 
 
-setTimeout(() => {
-  console.log('despues de 4 segundos')
-}, 4000);
+// setTimeout(() => {
+//   console.log('despues de 4 segundos')
+// }, 4000);
+
+const saludar = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('Hola mundo. Soy llamado desde una promesa');
+    reject('Ha fallado la promesa');
+  }, 2000);
+})
+
+console.log(saludar)
+
+saludar.then((res) => {
+  console.log(res)
+}).catch((err) => {
+  console.log(err)
+})
+
+
+
+
